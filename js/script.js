c786c14f9f047315ad7c30fcd90ea94808689f8b
Charles Ivia <iviacharles@gmail.com>
2021-11-03 09:35:33 +0300

    creating a kanban item logic

---
+++ js/script.js
@@ -0,0 +1,64 @@
+//Fetch dom elments
+const error = document.querySelector(".error");
+const erroMessage = "Please add details!";
+const addBtn = document.querySelector(".add");
+let adding = false;
+let order = 1;
+
+addBtn.addEventListener("click", () => {
+  const target = document.querySelector("#requested");
+  if (adding == false) {
+    adding = true;
+    target.appendChild(createItem());
+  } else {
+    error.innerHTML = erroMessage;
+  }
+});
+
+//Functions
+
+let createItem = () => {
+  let item = document.createElement("div");
+  //item.innerHTML = "Bitch please";
+  item.classList.add("item");
+  item.setAttribute("id", `item-${order}`);
+  item.setAttribute("draggable", "true");
+
+  //dragstart event listener
+  item.addEventListener("dragstart", (e) => {
+    return e.dataTransfer.setData("text", e.target.id);
+  });
+
+  //dragend event listener
+
+  item.addEventListener("dragend", (e) => {
+    return e.dataTransfer.clearData();
+  });
+
+  //Input element
+
+  let input = document.createElement("input");
+  item.appendChild(input);
+
+  //Save button
+
+  let saveBtn = document.createElement("button");
+  saveBtn.innerHTML = "Save";
+
+  //Save button event listener and validation
+  saveBtn.addEventListener("click", () => {
+    error.innerHTML = " ";
+    if (input.value !== "") {
+      order = order + 1;
+      item.innerHTML = input.value;
+      adding = false;
+    } else {
+      error.innerHTML = erroMessage;
+    }
+  });
+
+  //Append the save button
+  item.appendChild(saveBtn);
+
+  return item;
+};
